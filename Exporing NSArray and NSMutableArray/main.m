5a50d2d1f1455c1f06a145e2e0a6b0ed4b070568
Chu Nam Anh <khuyendaxoa1996@yahoo.com.vn>
2016-06-12 14:55:34 +0700

    Complete ( Kind of )

--- Exporing NSArray and NSMutableArray/main.m
+++ Exporing NSArray and NSMutableArray/main.m
@@ -8,10 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSArray *a = [NSArray arrayWithObjects:@"object 1",@"object 2",@"holy hell",nil];
+        
+        NSMutableArray *b = [[NSMutableArray alloc] init];
+        NSDate *now = [NSDate date];
+        NSDate *yesterday = [now dateByAddingTimeInterval:-24.0*60.0*60.0];
+        NSArray *datalist = [NSArray arrayWithObjects:now,yesterday, nil];
+        
+        // Cach dung NSArray
+        [a containsObject:@"object 2"] ? NSLog(@"Yes") : NSLog(@"no");
+        NSLog(@"So phan tu trong mang la: %ld",[a count]);
+        NSLog(@"Phan tu cuoi cung : %@",[a lastObject]);
+        for (int i=0;i < [a count];i++)
+        {
+            NSLog(@" Phan tu thu %d la : %@",i+1,[a objectAtIndex:i]);
+        }
+        
+        // Cach dung NSMutable Array
+        [b addObjectsFromArray:a];
+        for (int i=0;i<[datalist count];i++)
+        {
+            [b addObject:[datalist objectAtIndex:i]];
+        }
+        for (int i=0;i<[b count];i++)
+        {
+            NSLog(@" Phan tu thu %d la : %@",i+1,[b objectAtIndex:i]);
+        }
     }
     return 0;
 }
